696ee37d134aea6dcd33ad90791c9db5fc0c6173
Prabha <pamarasinghe@ufl.edu>
2015-10-27 12:23:35 -0400

    add data
    changed threshold

--- tsanalysissizeclass.R
+++ tsanalysissizeclass.R
@@ -4,4 +4,12 @@ get_data <- function(){
 }
 
 
-
+git_size_class <- function(weight,data){
+  if(weight>data){
+    size_class <- "large"
+  }
+  else{
+    size_class <- "small"
+  }
+  return(size_class)
+}
